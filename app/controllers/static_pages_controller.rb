4dcd86074b5ab1e4b2ff2272a457187ce46724db
Evan A <evjay.a@gmail.com>
2014-12-22 14:50:15 -0700

    built up the basic structure of the views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,16 @@
 class StaticPagesController < ApplicationController
+  before_action :list_links, only:(:home)
 
   def home
   end
 
   def about
   end
+
+  private
+
+    def list_links
+      @list_links = Link.all
+    end
 
 end
