0e2f6ce564a96139732df3ae458f3a855e2599ac
Evan A <evjay.a@gmail.com>
2014-12-22 14:20:08 -0700

    added associations and migrations. fixed up the controllers and routes, now startin on the views

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222203720) do
+ActiveRecord::Schema.define(version: 20141222211053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: true do |t|
+    t.string   "title"
+    t.text     "url"
+    t.boolean  "votes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
